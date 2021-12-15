bb27b9496e3dcde747d38d0932e1a4f189c88354
darya.sukhova <darya.sukhova@corp.mail.ru>
2021-12-15 11:02:10 +0300

    работает автосоздание таблицы

--- install.sh
+++ install.sh
@@ -10,7 +10,4 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-#sudo groupadd docker && sudo usermod -aG docker $USER && sudo service docker start
-#sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose; sudo chmod 0755 /usr/bin/docker-compose
 docker-compose -f docker-compose-non-dev.yml up
