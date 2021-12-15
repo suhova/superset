a2a2ae2c5cddd2d9e012fe8f40325062c70cd53b
darya.sukhova <darya.sukhova@corp.mail.ru>
2021-12-15 11:28:51 +0300

    работает автосоздание таблицы

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!bin/bash
+sudo chmod 755 ./
 sudo apt-get update
 sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod 755 /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo systemctl start docker
 sudo docker-compose -f docker-compose-non-dev.yml up
