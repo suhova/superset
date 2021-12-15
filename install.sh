e855c974ff3981840764f2253f5571c5a822e0a4
darya.sukhova <darya.sukhova@corp.mail.ru>
2021-12-15 11:50:17 +0300

    работает автосоздание таблицы

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
 #!bin/bash
-sudo chmod 755 ./
 sudo apt-get update
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod 755 /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo apt install linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update && apt-cache policy docker-ce
+sudo apt install -y docker-ce
+sudo usermod -aG docker $(whoami)
+sudo systemctl status docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 sudo systemctl start docker
 sudo docker-compose -f docker-compose-non-dev.yml up
