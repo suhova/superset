ba406aba4e82fae49cfda90bfa891f53b49d1d95
darya.sukhova <darya.sukhova@corp.mail.ru>
2021-12-15 11:58:11 +0300

    работает автосоздание таблицы

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo apt update && apt-cache policy docker-ce
 sudo apt install -y docker-ce
 sudo usermod -aG docker $(whoami)
 sudo systemctl status docker
+sudo systemctl start docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo systemctl start docker
 sudo docker-compose -f docker-compose-non-dev.yml up
